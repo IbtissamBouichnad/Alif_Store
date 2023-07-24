9addb12ad204ef55b94e50fcefd32840c75e8236
IbtissamBouichnad <ibtissam.bouichnad@gmail.com>
2023-07-24 20:58:34 +0100

    four

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -36,19 +36,19 @@ export class HeaderComponent {
     this.toggle3 = false;
   }
   cards=[
-    {status:false,image:'/assets/Picture2.png',titre:'3 PROHETES ET 1 LIVRET',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
-    {status:false,image:'/assets/Picture2.png',titre:'3 RECITS AUDIO',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
-    {status:false,image:'/assets/Picture2.png',titre:'LIVRET 3',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
-    {status:false,image:'/assets/Picture2.png',titre:'LIVRET 4',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
+    {status:false,image:'assets/Picture2.png',titre:'3 PROHETES ET 1 LIVRET',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
+    {status:false,image:'assets/Picture2.png',titre:'3 RECITS AUDIO',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
+    {status:false,image:'assets/Picture2.png',titre:'LIVRET 3',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
+    {status:false,image:'assets/Picture2.png',titre:'LIVRET 4',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
   ]
   pack=[
-    {status:false,image:'/assets/Picture2.png',titre:'3 PROHETES ET 1 LIVRET',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
+    {status:false,image:'assets/Picture2.png',titre:'3 PROHETES ET 1 LIVRET',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
   ]
   livrets=[
-    {status:false,image:'/assets/Picture2.png',titre:'3 PROHETES ET 1 LIVRET',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
-    {status:false,image:'/assets/Picture2.png',titre:'3 RECITS AUDIO',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
-    {status:false,image:'/assets/Picture2.png',titre:'LIVRET 3',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
-    {status:false,image:'/assets/Picture2.png',titre:'LIVRET 4',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
+    {status:false,image:'assets/Picture2.png',titre:'3 PROHETES ET 1 LIVRET',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
+    {status:false,image:'assets/Picture2.png',titre:'3 RECITS AUDIO',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
+    {status:false,image:'assets/Picture2.png',titre:'LIVRET 3',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
+    {status:false,image:'assets/Picture2.png',titre:'LIVRET 4',age:'A paritr de 3 ans',description:'Offrir aux enfants la possibilité dexplorer des récits prophétiques authentiques de l Islam',prix:'99,00€'},
   ]
 
 
